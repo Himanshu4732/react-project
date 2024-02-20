87aa90e62c2208ba0644ff0cdb56fdfda11fae26
himanshu4732 <118583527+Himanshu4732@users.noreply.github.com>
2024-02-20 17:23:15 +0530

    Update Cards.jsx adding scrolling

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,23 +4,24 @@ import { LuDownload } from "react-icons/lu";
 import { IoIosCloseCircle } from "react-icons/io";
 import { motion } from "framer-motion";
 
-function Cards({ data }) {
+function Cards({ data, reference }) {
   console.log("Data in Cards component:", data); // Log data to ensure it's correctly received
 
   return (
     <motion.div
       drag
       whileDrag={{ scale: 1.2 }}
-      dragTransition={{ bounceStiffness: 100, bounceDamping: 7 }}
-      className="relative w-52 h-72 rounded-[35px] border-slate-600 border bg-zinc-900/90 px-8 py-10 text-white overflow-hidden"
+      dragConstraints={reference}
+      dragTransition={{ bounceStiffness: 300, bounceDamping: 10 }}
+      className="relative w-52 h-72 rounded-[35px] border-slate-600 border bg-zinc-900/90 px-6 py-7 text-white overflow-hidden"
     >
       <FaRegFileAlt />
-      <p className="text-sm font-semibold mt-5 leading-tight">
+      <p className="text-sm w-full h-28 font-semibold mt-5 leading-tight overflow-auto no-scrollbar">
         {data.description}
       </p>
       <div className="footer absolute bottom-0 left-0 w-full">
-        <div className="flex items-center justify-center px-8 py-3 mb-3">
-          <div className="flex items-center justify-between mb-5 w-full">
+        <div className="flex items-center justify-center px-5 py-3 mb-3">
+          <div className="flex items-center justify-between  w-full">
             <h5>{data.size}MB</h5>
             {data.tag ? (
               <IoIosCloseCircle className="cursor-pointer" size="1.2rem" />
